05d3afe93cbbfe420334cc4bad5e0b1a71a92cdc
Klik2 <rayandausbr@gmail.com>
2026-02-11 02:56:54 +0700

    Delete styles/arabicNumbers.ts

--- styles/arabicNumbers.ts
+++
@@ -1,4 +0,0 @@
-export const toArabicNumerals = (num: number): string => {
-  const arabicDigits = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
-  return num.toString().split('').map(digit => arabicDigits[parseInt(digit)]).join('');
-};
